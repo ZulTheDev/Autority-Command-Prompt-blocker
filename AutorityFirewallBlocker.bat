c3363597f59c3742499a525b032043492b2bbf87
Zul <76121296+ZulTheDev@users.noreply.github.com>
2021-03-29 19:04:17 +0800

    AuthoritySchoolterminalBlocker
    Hello guys! I Am 14yr Programmer and I Want to share some tool I Made!
    this is just A Tool to prevent children from entering the command prompt as I Am Going to make A Blocker for window PowerShell as well!

---
+++ AutorityFirewallBlocker.bat
@@ -0,0 +1,52 @@
+@echo off
+Title Authority Firewall blocker
+color fc
+:home
+echo                                               hhhhhh
+echo                                             mhhhhhhhhm
+echo                                            mhhhhhhhhhhm
+echo                                           dhhhhhhhhhhhhd
+echo                                          dhhhhhhhhhhhhhhd
+echo                                         hhhhhhhhhhhhhhhhhh
+echo                                        hhhhhhhhhhhhhhhhhhhh
+echo                                      mhhhhhhhhhh//hhhhhhhhhh
+echo                                     mhhhhhhhhhy:  :yhhhhhhhhhm
+echo                                    mhhhhhhhhhy-    -yhhhhhhhhhm                        Hello student! We Apologies for Inconvience issue with this command!
+echo                                   dhhhhhhhhhy.      -yhhhhhhhhhd                       You should not be using this tool to excute/perform the hack!
+echo                                  dhhhhhhhhhs.        .shhhhhhhhhd                      Thank you for Your understanding! Press Key "X" to Exit
+echo                                 hhhhhhhhhho`          `ohhhhhhhhhh
+echo                                hhhhhhhhhh+`   `---.`   `+hhhhhhhhhh
+echo                              mhhhhhhhhhy/   `omNNNNmo`   /yhhhhhhhhhm                  
+echo                             mhhhhhhhhhy:    sMMMMMMMMs    :yhhhhhhhhhm
+echo                            dhhhhhhhhhy-     dMMMMMMMMd     -yhhhhhhhhhd
+echo                           dhhhhhhhhhs.      dMMMMMMMMh      .shhhhhhhhhd               
+echo                          dhhhhhhhhhs`       yMMMMMMMMy       .shhhhhhhhhd
+echo                         hhhhhhhhhho`        oMMMMMMMMo        `ohhhhhhhhhh
+echo                        hhhhhhhhhh+`         /MMMMMMMM/         `+hhhhhhhhhh
+echo                      mhhhhhhhhhy/           -MMMMMMMM-           /yhhhhhhhhhm
+echo                     mhhhhhhhhhy:            `MMMMMMMM`            :yhhhhhhhhhm
+echo                    dhhhhhhhhhy-              NMMMMMMN              -yhhhhhhhhhd
+echo                   dhhhhhhhhhs.               dMMMMMMd               .shhhhhhhhhd
+echo                  hhhhhhhhhho`                yMMMMMMy                `shhhhhhhhhd
+echo                 hhhhhhhhhho`                 oMMMMMMo                 `ohhhhhhhhhh
+echo                hhhhhhhhhy+                   -MMMMMM-                   +yhhhhhhhhh
+echo              mhhhhhhhhhy/                     sNNNNo                     /yhhhhhhhhhm
+echo             mhhhhhhhhhy:                       .--.                       :yhhhhhhhhhm
+echo            dhhhhhhhhhy-                        ````                        -yhhhhhhhhhd
+echo           dhhhhhhhhhs.                       :ydmmdy:                       .shhhhhhhhhd
+echo          hhhhhhhhhho`                       +MMMMMMMM+                       `ohhhhhhhhhh
+echo         hhhhhhhhhh+`                        mMMMMMMMMm                        `ohhhhhhhhhh
+echo       mhhhhhhhhhy/                          +NMMMMMMN+                          +yhhhhhhhhh
+echo      mhhhhhhhhhy:                            -sdNNds-                            :yhhhhhhhhhm
+echo     dhhhhhhhhhy-                                                                  -yhhhhhhhhhm
+echo    dhhhhhhhhhh+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::+hhhhhhhhhhd
+echo   dhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhd
+echo  hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+echo hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhm
+echo ddhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhp                           --ADMINSTRATOR EXIT!
+echo
+echo          We are blocking this incase if you are the one who love to hack!
+echo          Sorry! back to your assignment! 
+echo         |The Random school blocker!| --change this to your organization or school
+start chrome.exe -any browser that most of them uses After that erase of of this commenting! to mkae this work!
+choice /c x /M "Sorry, you Are Not Autorize to use this tool! Please go to the helpdesk to solve this problem."
